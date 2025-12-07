531d1bfd0e4a50b53b3517dc199c419b5b17aa46
Lars Windolf <lars.windolf@gmx.de>
2025-12-07 21:34:22 +0100

    Add SSDP/Bonjour discovery

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // vim: set ts=4 sw=4:
 /*jshint esversion: 6 */
 /*
-  Copyright (C) 2015-2024  Lars Windolf <lars.windolf@gmx.de>
+  Copyright (C) 2015-2025  Lars Windolf <lars.windolf@gmx.de>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -43,6 +43,15 @@ process.on('uncaughtException', function (err) {
 const validIpAddressRegex = /^([a-zA-Z0-9]+@){0,1}(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
 const validHostnameRegex = /^([a-zA-Z0-9]+@){0,1}(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
 
+function get_status(socket) {
+	socket.send(JSON.stringify({
+		cmd: 'status',
+		result: Object.entries(proxies).map(([k,v]) => {
+			return { name: k, status: v.getStatus() }
+		})
+	}));
+}
+
 // get history of kubectl contexts
 function get_kubectxt(socket) {
 	try {
@@ -53,7 +62,7 @@ function get_kubectxt(socket) {
 
 			socket.send(JSON.stringify({
 				cmd: 'kubectxt',
-				result: stdout
+				result: JSON.parse(stdout)
 			}));
 		});
 	} catch (e) {
@@ -85,7 +94,7 @@ function get_history(socket) {
 // get all hosts currently SSH connected
 function get_hosts(socket) {
 	try {
-		const cmd = `pgrep -fla "^ssh " || true`;
+		const cmd = 'pgrep -fla "^ssh " || true';
 		exec(cmd, (error, stdout, stderr) => {
 			if (error)
 				throw (error);
@@ -200,11 +209,49 @@ function getProxy(host) {
 	return proxies[host];
 }
 
+// discover local network services via mDNS and SSDP
+function get_localnet(socket) {
+	if (undefined === proxies[':localnet_discover']) {
+		proxies[':localnet_discover'] = new StatefulProcessCommandProxy({
+			name: ':localnet_discover',
+			max: 1,
+			min: 1,
+			idleTimeoutMS: 60000,
+			logFunction: function (severity, origin, msg) {
+				//console.log(severity.toUpperCase() + " " +origin+" "+ msg);
+			},
+			processCommand: "/bin/bash",
+			processArgs: [],
+			processRetainMaxCmdHistory: 0,
+			processInvalidateOnRegex: {
+				//'stderr':[{regex:'.*error.*',flags:'ig'}]
+			},
+			processCwd: './',
+			processUid: null,
+			processGid: null,
+			initCommands: ['LANG=C;echo'],	// to catch banners and pseudo-terminal warnings
+			validateFunction: function (processProxy) {
+				return processProxy.isValid();
+			},
+		});
+	}
+
+	getProxy(':localnet_discover').executeCommands([
+		'node scripts/local-network-discover.mjs'
+	]).then(function (res) {
+		socket.send(JSON.stringify({
+			cmd: 'localnet',
+			result: JSON.parse(res[0].stdout)
+		}));
+	}).catch(function (e) {
+		return { cmd: 'localnet', error: e };
+	});
+}
+
 function probeWS(socket, host, probe) {
 	try {
-		if (!(probe in probes)) {
-			return { host: host, probe: probe, error: 'No such probe' };
-		}
+		if (!(probe in probes))
+			return { host, probe, error: 'No such probe' };
 
 		getProxy(host).executeCommands([probes[probe].command]).then(function (res) {
 			let msg = {
@@ -216,9 +263,9 @@ function probeWS(socket, host, probe) {
 				next: []
 			};
 
-			if ('name' in probes[probe]) msg.name = probes[probe].name;
+			if ('name'   in probes[probe]) msg.name   = probes[probe].name;
 			if ('render' in probes[probe]) msg.render = probes[probe].render;
-			if ('type' in probes[probe]) msg.type = probes[probe].type;
+			if ('type'   in probes[probe]) msg.type   = probes[probe].type;
 			// Suggest followup probes
 			for (let p in probes) {
 				if (probes[p]['if'] === probe && -1 !== res[0].stdout.indexOf(probes[p].matches))
@@ -287,7 +334,7 @@ wsServer.on('connection', (ws, req, client) => {
 	// Send a version so frontend can check for compatibility
 	ws.send(JSON.stringify({
 		cmd: 'version',
-		value: '0.9.1'
+		value: '0.9.11'
 	}));
 
 	ws.on('message', function (message) {
@@ -325,6 +372,10 @@ wsServer.on('connection', (ws, req, client) => {
 				return get_history(ws);
 			if (cmd === 'kubectxt')
 				return get_kubectxt(ws);
+			if (cmd === 'localnet')
+				return get_localnet(ws);
+			if (cmd === 'status')
+				return get_status(ws);
 
 			if (cmd === 'run') {
 				// we expect message in format "run <host>:::<id>:::<cmd>"
